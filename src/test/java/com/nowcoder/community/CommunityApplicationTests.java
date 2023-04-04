d53197205509aca2ffa8f8b090ba3d9358c86b7c
deerks <djqdks1@mail.ustc.edu.cn>
2023-04-04 20:53:40 +0800

    Spring MVC入门

--- src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -1,12 +1,17 @@
 package com.nowcoder.community;
 
 import com.nowcoder.community.dao.AlphaDao;
+import com.nowcoder.community.service.AlphaService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootTest
 @ContextConfiguration(classes = CommunityApplication.class)
@@ -29,5 +34,23 @@ public class CommunityApplicationTests implements ApplicationContextAware {
 		AlphaDao alphaDao = applicationContext.getBean(AlphaDao.class);
 		System.out.println(alphaDao.select());
 	}
+	@Test
+	public void testBeanManagement() {
+		AlphaService alphaService = applicationContext.getBean(AlphaService.class);
+		System.out.println(alphaService);
+		alphaService = applicationContext.getBean(AlphaService.class);
+		System.out.println(alphaService);
+	}
 
+	@Test
+	public void testBeanConfig() {
+		SimpleDateFormat simpleDateFormat = applicationContext.getBean(SimpleDateFormat.class);
+		System.out.println(simpleDateFormat.format(new Date()));
+	}
+	@Autowired
+	private AlphaDao alphaDao;
+	@Test
+	public void testDI() {
+		System.out.println(alphaDao);
+	}
 }
