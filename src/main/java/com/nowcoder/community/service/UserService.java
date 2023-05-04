6d0e18d810e9babf3ba57d8bb41ce62772a346db
deerks <djqdks1@mail.ustc.edu.cn>
2023-05-04 17:15:06 +0800

    上传头像

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -159,4 +159,8 @@ public class UserService implements CommunityConstant {
         return loginTicketMapper.selectByTicket(ticket);
     }
 
+    public int updateHeader(int userId, String headUrl) {
+        return userMapper.updateHeader(userId, headUrl);
+    }
+
 }
