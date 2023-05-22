d29d95b6227e2b129102bbf99cd817aa22baf308
deerks <djqdks1@mail.ustc.edu.cn>
2023-05-22 22:48:10 +0800

    实现点赞功能，Redis使用

---
+++ src/main/java/com/nowcoder/community/service/LikeService.java
@@ -0,0 +1,37 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.util.RedisKeyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LikeService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    //点赞
+    public void like(int userId, int entityType, int entityId) {
+        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+        boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);
+        if(isMember) {
+            redisTemplate.opsForSet().remove(entityLikeKey, userId);
+        } else {
+            redisTemplate.opsForSet().add(entityLikeKey, userId);
+        }
+    }
+
+    //查询某实体获得赞的数量
+    public long findEntityLikeCount(int entityType, int entityId) {
+        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+        return redisTemplate.opsForSet().size(entityLikeKey);
+    }
+
+    //查询某用户对某实体是否点过赞
+    public int findEntityLikeStatus(int userId, int entityType, int entityId) {
+        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+        //已点赞返回1，未点赞返回0
+        return redisTemplate.opsForSet().isMember(entityLikeKey, userId) ? 1 : 0;
+    }
+}
