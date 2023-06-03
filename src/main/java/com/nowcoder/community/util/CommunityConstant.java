46d7b53c165a0f9c89fcbcddcddfbfb5a345bf87
deerks <djqdks1@mail.ustc.edu.cn>
2023-06-03 19:36:21 +0800

    封装事件，定义生产者和消费者

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -1,5 +1,7 @@
 package com.nowcoder.community.util;
 
+import org.apache.kafka.common.protocol.types.Field;
+
 public interface CommunityConstant {
 
     /**
@@ -35,4 +37,21 @@ public interface CommunityConstant {
      * 实体类型：评论
      */
     int ENTITY_TYPE_USER = 3;
+
+    /**
+     * 主题：评论
+     */
+    String TOPIC_COMMENT = "comment";
+    /**
+     * 主题：点赞
+     */
+    String TOPIC_LIKE = "like";
+    /**
+     * 主题：关注
+     */
+    String TOPIC_FOLLOW = "follow";
+    /**
+     * 系统用户ID
+     */
+    int SYSTEM_USER_ID = 1;
 }
